ffe5916c35a4eed208c0c58f7ed1457362b8e598
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-02-06 16:54:54 +0100

    #7 Manage custom properties based portlet.properties files

---
+++ docroot/WEB-INF/src/com/liferay/jira/metrics/util/PortletPropsKeys.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jira.metrics.util;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class PortletPropsKeys {
+
+	public static final String JIRA_PASSWORD = "jira.password";
+
+	public static final String JIRA_SERVER_URI = "jira.server.uri";
+
+	public static final String JIRA_USERNAME = "jira.username";
+
+}
