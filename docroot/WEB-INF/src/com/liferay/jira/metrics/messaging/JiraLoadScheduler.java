2891de97829676e0bdc75af2fa2c539f713a27a0
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-02-06 16:39:36 +0100

    #2 Create a basic scheduler, that is triggered at 00:01, everyday

---
+++ docroot/WEB-INF/src/com/liferay/jira/metrics/messaging/JiraLoadScheduler.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jira.metrics.messaging;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class JiraLoadScheduler {
+
+	public static void load() {
+		_log.info("Loading data from JIRA...");
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(JiraLoadScheduler.class);
+
+}
