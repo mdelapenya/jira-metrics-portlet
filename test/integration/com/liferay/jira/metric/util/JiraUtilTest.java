e9f739e54d548cf404a79b333a66039709786616
cgoncas <cristina.gonzalez@liferay.com>
2014-02-27 09:55:49 +0100

    Conflicts: 	test/integration/com/liferay/jira/metric/util/JiraUtilTest.java

--- test/integration/com/liferay/jira/metric/util/JiraUtilTest.java
+++ test/integration/com/liferay/jira/metric/util/JiraUtilTest.java
@@ -15,13 +15,28 @@
 package com.liferay.jira.metric.util;
 
 import com.atlassian.jira.rest.client.RestClientException;
+import com.atlassian.jira.rest.client.domain.BasicComponent;
 import com.atlassian.jira.rest.client.domain.BasicProject;
 import com.atlassian.jira.rest.client.domain.Project;
 import com.atlassian.jira.rest.client.domain.Status;
+import com.atlassian.jira.rest.client.domain.Component;
+import com.atlassian.jira.rest.client.domain.Project;
+import com.atlassian.jira.rest.client.domain.Status;
+
+import com.google.common.collect.Lists;
+
+import com.liferay.jira.metrics.util.IssuesMetric;
 import com.liferay.jira.metrics.util.JiraUtil;
 import com.liferay.jira.metrics.util.PortletPropsKeys;
 import com.liferay.jira.metrics.util.PortletPropsUtil;
 
+import java.util.List;
+
+
+import java.net.URI;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.After;
@@ -29,6 +44,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
@@ -67,9 +83,90 @@ public class JiraUtilTest  extends PowerMockito {
 		Assert.assertTrue(projects.size() > 0);
 	}
 
-	@Test
 	public void getAllJiraStatuses() throws Exception {
-		List<Status> jiraStatuses = JiraUtil.getAllJiraStatuses();
+		List<Status> statuses = JiraUtil.getAllJiraStatuses();
+
+		Assert.assertNotNull(statuses);
+
+		Assert.assertTrue(statuses.size() > 0);
+	}
+
+	@Test
+	public void getComponent() throws Exception {
+		Project project = JiraUtil.getProject(_PROJECT_KEY);
+
+		Iterable<BasicComponent> components = project.getComponents();
+		Iterator<BasicComponent> componentsIterator = components.iterator();
+
+		BasicComponent basicComponent = componentsIterator.next();
+
+		Component component = JiraUtil.getComponent(basicComponent.getSelf());
+
+		Assert.assertNotNull(component);
+
+		Assert.assertNotNull(component.getSelf());
+
+		Assert.assertEquals(basicComponent.getSelf(), component.getSelf());
+
+		Assert.assertEquals(basicComponent.getName(), component.getName());
+
+		Assert.assertEquals(
+			basicComponent.getDescription(), component.getDescription());
+	}
+
+	@Test
+	public void getIssuesMetricsByProjectStatus() throws Exception {
+		Project project = JiraUtil.getProject(_PROJECT_KEY);
+
+		Iterable<BasicComponent> basicComponentsIterable =
+			project.getComponents();
+
+		List<BasicComponent> basicComponentsList = Lists.newArrayList(
+			basicComponentsIterable);
+
+		Status status = JiraUtil.getStatus(new URI(_STATUS_URI));
+
+		List<Status> statuses = new ArrayList<Status>();
+
+		statuses.add(status);
+
+		List<IssuesMetric> jiraMetrics =
+			JiraUtil.getIssuesMetricsByProjectStatus(project, statuses);
+
+		Assert.assertEquals(
+			basicComponentsList.size() * _PRIORITY_SIZE, jiraMetrics.size());
+	}
+
+	@Test
+	public void getProject() throws Exception {
+		Project project = JiraUtil.getProject(_PROJECT_KEY);
+
+		Assert.assertNotNull(project);
+
+		Assert.assertNotNull(project.getKey());
+
+		Assert.assertEquals(_PROJECT_KEY, project.getKey());
+	}
+
+	@Test
+	public void getStatus() throws Exception {
+		List<Status> statuses = JiraUtil.getAllJiraStatuses();
+
+		Status status = statuses.get(0);
+
+		Status currentStatus = JiraUtil.getStatus(status.getSelf());
+
+		Assert.assertNotNull(currentStatus);
+
+		Assert.assertEquals(status.getName(), currentStatus.getName());
+
+		Assert.assertEquals(
+			status.getDescription(), currentStatus.getDescription());
+	}
+
+	@Before
+	public void setUp() {
+		mockStatic(PortletPropsUtil.class);
 
 		Assert.assertNotNull(jiraStatuses);
 		Assert.assertTrue(jiraStatuses.size() > 0);
@@ -102,5 +199,12 @@ public class JiraUtilTest  extends PowerMockito {
 			TestPropsUtil.getValue(key)
 		);
 	}
+
+	private static final int _PRIORITY_SIZE = 5;
+
+	private static final String _PROJECT_KEY = "LPS";
+
+	private static final String _STATUS_URI =
+		"https://issues.liferay.com/rest/api/2/status/3";
 
 }
