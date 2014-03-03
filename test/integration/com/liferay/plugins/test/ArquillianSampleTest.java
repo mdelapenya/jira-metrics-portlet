fc122a26504fe3be776531cac3e0747b03ff5295
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-03-03 10:26:14 +0100

    Support Arquillian in tests #8

---
+++ test/integration/com/liferay/plugins/test/ArquillianSampleTest.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.plugins.test;
+
+import org.jboss.arquillian.junit.Arquillian;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Manuel de la Peña
+ */
+@RunWith(Arquillian.class)
+public class ArquillianSampleTest extends BaseArquillianTestCase{
+
+	@Test
+	public void test1() {
+		System.out.println("First test with Arquillian!");
+	}
+
+}
