bf604d6a52376c74cad02f5cf7ae0973797b8641
cgoncas <cristina.gonzalez@liferay.com>
2014-03-06 13:26:48 +0100

    #5 New debug log message

--- docroot/WEB-INF/src/com/liferay/jira/metrics/util/JiraUtil.java
+++ docroot/WEB-INF/src/com/liferay/jira/metrics/util/JiraUtil.java
@@ -127,9 +127,17 @@ public class JiraUtil {
 		for (String statusName : statusNames) {
 			for (BasicComponent component : components) {
 				for (Priority priority : priorities) {
+
 					int total =
 						getIssuesMetricsByProjectStatusComponentPriority(
 							project, statusName, component, priority);
+
+					if(_log.isDebugEnabled()) {
+						_log.debug(
+							"[" + project.getKey() + "]" + "[" +
+								component.getName() + "]" + "[" + statusName +
+								"]" + "["+priority.getId() + "] = " + total);
+					}
 
 					results.add(
 						new IssuesMetric(
